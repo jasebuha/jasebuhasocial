648473e72152e6d6d20b134e8975b8ad390730f6
jasebuha <jasebuha@hotmail.com>
2017-02-28 07:37:51 -0800

    Sign in ui custom classes complete

---
+++ jasebuhasocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  jasebuhasocial
+//
+//  Created by Jason Buhagiar on 2/28/17.
+//  Copyright © 2017 Jason Buhagiar. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ jasebuhasocial/FancyButton.swift
@@ -0,0 +1,24 @@
+//
+//  FancyButton.swift
+//  jasebuhasocial
+//
+//  Created by Jason Buhagiar on 2/28/17.
+//  Copyright © 2017 Jason Buhagiar. All rights reserved.
+//
+
+import UIKit
+
+class FancyButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 2.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+
+    
+
+}
